0e38b11a817400702a41b981e835954055a91607
Alexander Urlich <ulrichalex12@yahoo.de>
2022-07-04 18:25:42 +0200

    peparing menu Overlay

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,1 +1,7 @@
-console.log('Testing JS Script File');
+const hamBtn = document.querySelector('#btn-ham');
+const header = document.querySelector('#header');
+
+hamBtn.addEventListener('click', () => {
+  console.log('Hamburger clicked');
+  header.classList.toggle('open');
+});
