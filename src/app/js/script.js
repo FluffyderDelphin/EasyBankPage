af7a515dd1a1b3f1e3794c85c0e232ee156e67c9
Alexander Urlich <ulrichalex12@yahoo.de>
2022-07-06 02:09:34 +0200

    added Fade to Menu

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,19 +1,29 @@
 const hamBtn = document.querySelector('#btn-ham');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const hiddenElements = document.querySelectorAll('.hideable');
 
 hamBtn.addEventListener('click', () => {
   console.log('Hamburger clicked');
   if (header.classList.contains('open')) {
+    hiddenElements.forEach((e) => {
+      e.classList.remove('fade-in');
+      e.classList.add('fade-out');
+    });
+
     setTimeout(() => {
       header.classList.remove('open');
+      hiddenElements.forEach((e) => {
+        e.classList.add('hidden');
+      });
     }, 300);
-
-    overlay.classList.add('fade-out');
-    overlay.classList.remove('fade-in');
   } else {
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+
+    hiddenElements.forEach((e) => {
+      e.classList.remove('hidden');
+      e.classList.add('fade-in');
+      e.classList.remove('fade-out');
+    });
   }
 });
